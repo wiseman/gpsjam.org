4cc34fcdb5d56b056a6146da321a8d1db16d02e1
John Wiseman <jjwiseman@gmail.com>
2022-07-03 14:32:20 -0700

    Don't sync .git.

--- bin/sync.sh
+++ bin/sync.sh
@@ -7,4 +7,4 @@ public_dir=$base_dir/../
 chmod a-x ${public_dir}/data/*.csv
 chmod a+r ${public_dir}/data/*.csv
 remote="heavymeta.org:/var/www/lab.heavymeta.org/html/gpsjam"
-rsync -av --info=progress2 --checksum "$public_dir" "$remote"
+rsync -av --info=progress2 --checksum --exclude='.git/' "$public_dir" "$remote"
