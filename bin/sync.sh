4dd30c1a074422dd23e756538327e2f2cc19cc08
John Wiseman <jjwiseman@gmail.com>
2022-07-05 11:01:50 -0700

    Added more data. Render red hexes first.

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-set -x
 
 base_dir="$(dirname "$0")"
 public_dir=$base_dir/../
-chmod a-x ${public_dir}/data/*.csv
-chmod a+r ${public_dir}/data/*.csv
+chmod a-x "${public_dir}"/data/*.csv
+chmod a+r "${public_dir}"/data/*.csv
 remote="heavymeta.org:/var/www/lab.heavymeta.org/html/gpsjam"
 rsync -av --info=progress2 --checksum --exclude='.git/' "$public_dir" "$remote"
