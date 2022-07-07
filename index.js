511bcfcddbca0faa7340a90a3433e36e5879e291
John Wiseman <jjwiseman@gmail.com>
2022-07-06 23:13:28 -0700

    Use port from env var if specified.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 const { url } = require('inspector');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app = express();
 
