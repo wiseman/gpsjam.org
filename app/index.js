513158a0296685014a9e435d6d853ef9fc8e7daa
John Wiseman <jjwiseman@gmail.com>
2022-07-19 14:37:07 -0700

    Added preview image caching.

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path')
 
 const express = require("express");
+const LRU = require('lru-cache');
 const puppeteer = require("puppeteer");
 
 const PORT = process.env.PORT || 3000;
@@ -45,38 +46,69 @@ function delay(t, val) {
     });
 }
 
+const PREVIEW_CACHE = LRU({
+    max: 100,
+});
+let PREVIEW_BROWSER;
+let PREVIEW_BROWSER_USE_COUNT = 0;
+
 app.get("/preview", async (req, res) => {
     // Get the "u" query param.
     const urlStr = req.query.u;
     console.log('Screenshotting ' + urlStr);
-    const url = new URL(urlStr);
-    const params = url.searchParams;
-    const zoom = parseFloat(params.get('z')) - 1.0;
-    params.set('z', zoom.toString());
-    params.set('screenshot', '');
-    console.log(url);
+    let imageBuf;
+    if (PREVIEW_CACHE.has(urlStr)) {
+        console.log('Getting preview image from cache');
+        imageBuf = PREVIEW_CACHE.get(urlStr);
+    } else {
+        const url = new URL(urlStr);
+        console.log(url);
+        const params = url.searchParams;
+        const zoom = parseFloat(params.get('z')) - 1.0;
+        const lat = parseFloat(params.get('lat'));
+        const lon = parseFloat(params.get('lon'));
+        if ((lat && !lon) || (!lat && lon)) {
+            // Return a 404 if the query params are incomplete.
+            console.log('Incomplete query params.');
+            res.status(404).send('Incomplete query params');
+            return;
+        }
 
-    // launch a new headless browser
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
+        params.set('z', zoom.toString());
+        params.set('screenshot', '');
 
-    // set the viewport size
-    await page.setViewport({
-        width: 800,
-        height: 418,
-        deviceScaleFactor: 1,
-    });
+        if (!PREVIEW_BROWSER || PREVIEW_BROWSER_USE_COUNT > 10) {
+            console.log('Creating new browser');
+            if (PREVIEW_BROWSER) {
+                console.log('Closing old browser');
+                await PREVIEW_BROWSER.close();
+            }
+            PREVIEW_BROWSER = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+            PREVIEW_BROWSER_USE_COUNT = 0;
+        }
+        const page = await PREVIEW_BROWSER.newPage();
+        PREVIEW_BROWSER_USE_COUNT += 1;
 
-    // tell the page to visit the url
-    await page.goto(url.toString());
-    await delay(5000);
-    // take a screenshot and save it in the screenshots directory
-    const imageBuf = await page.screenshot();
-    // Return the imageBuf in the response. Set the mime-type too.
-    // close the browser
-    await browser.close();
+        // set the viewport size
+        await page.setViewport({
+            width: 800,
+            height: 418,
+            deviceScaleFactor: 1,
+        });
+
+        // tell the page to visit the url
+        await page.goto(url.toString());
+        await delay(5000);
+        // take a screenshot and save it in the screenshots directory
+        imageBuf = await page.screenshot();
+        PREVIEW_CACHE.set(urlStr, imageBuf);
+        // Return the imageBuf in the response. Set the mime-type too.
+        // close the browser
+        await page.close();
+    }
     res.setHeader('Content-Type', 'image/png');
     res.send(imageBuf);
 });
